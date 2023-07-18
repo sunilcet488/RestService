778c0286114ea5532dc8768eca6906e7d3adfb9f
Sunil Kumar Das <sunilcet488@gmail.com>
2023-07-19 01:24:32 +0530

    first commit

---
+++ src/main/java/com/abapskill/RestServiceDbApplication.java
@@ -0,0 +1,13 @@
+package com.abapskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceDbApplication.class, args);
+	}
+
+}
